f20689d3139dbde513477416622e899eef6cfeec
lutsiu <oleksandr.lutsiuk.1603@gmail.com>
2025-08-30 13:07:25 +0200

    Styles were also added to framer motion

--- src/app/components/footer/index.tsx
+++ src/app/components/footer/index.tsx
@@ -1,13 +1,24 @@
+"use client";
+
+import useTransition from "@/app/hooks/useTransition";
+import { motion } from "framer-motion";
 import FooterCopyright from "./FooterCopyright";
 import FooterNavigation from "./FooterNavigation";
 
 
 export default function Footer() {
+  const {initial, animate} = useTransition();
 
   return (
-    <footer className="bg-black/70 text-white flex flex-col mt-[8rem] lg:mt-[15rem] pb-[2rem]">
+    <motion.footer 
+      className="bg-black/70 text-white flex flex-col mt-[8rem] lg:mt-[15rem] pb-[2rem]"
+      initial={initial}
+      whileInView={animate}
+      viewport={{ once: true, amount: 0.2 }}   
+      transition={{ duration: 0.6, ease: "easeInOut" }}
+    >
       <FooterNavigation/>
       <FooterCopyright/>
-    </footer>
+    </motion.footer>
   )
 }
